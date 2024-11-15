9e9e7c10517e71f67b4bb7ffc891edac72998ede
mphiliseni <mphilisenialexmhlongo@gmail.com>
2024-11-15 19:32:00 +0200

    Initial commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        [HttpPost]
+        public IActionResult SendMessage(string message)
+        {
+            // Here you would implement logic to process the user's message and return a response.
+            string botResponse = $"Bot response to '{message}'";
+            return Json(new { response = botResponse });
+        }
+    }
+}
